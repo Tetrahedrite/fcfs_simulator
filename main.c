4242ab3174be9438f5d05d81e1056e4e86e0cfc7
Jisu Yu <det3652000@naver.com>
2023-11-01 00:09:17 +0900

    Feat: Initialize tasks and print all

--- main.c
+++ main.c
@@ -1,16 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <signal.h>
+#include "requirement.h"
+#include "list.h"
 
 #define PRINT_CLEAR_SEQUENCE() puts("\033[H\033[J")
 
 static int shouldStop = 0;
 
+static struct NODE* taskListQueue, readyQueue, runQueue, terminatedQueue;
+
 void doTick() {
+    // Print all the tasks
+    struct NODE* current = taskListQueue;
+    while (current != NULL) {
+        printf("PID: %d, TaskTime: %d, ArrivalTime: %d, WorkingTime: %d, WaitTime: %d, ReturnTime: %d\n",
+               current->nodePid, current->task, current->arrivalTime,
+               current->workingTime, current->waitTime, current->returnTime);
+        current = current->next;
+    }
     while (getchar() != '\n');
 }
 
 int main() {
+    if (!initTasks(&taskListQueue, 10)) {
+        perror("initTasks");
+        exit(1);
+    }
+
     while (!shouldStop) {
         PRINT_CLEAR_SEQUENCE();
         puts("FCFS simulator by Jisu Yu");
